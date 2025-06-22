438f4797e2bb205251a6236513122fb691bda3f7
faizluqman7 <faizluqman7@gmail.com>
2025-06-22 23:56:53 +0100

    Initial Commit

---
+++ HealthMYTests/HealthMYTests.swift
@@ -0,0 +1,16 @@
+//
+//  HealthMYTests.swift
+//  HealthMYTests
+//
+//  Created by Faiz Luqman on 22/06/2025.
+//
+
+import Testing
+
+struct HealthMYTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
